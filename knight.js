45397c37fc4218ce9ff7636725ab0eea89dfc003
SwartzJonah <swartzjonah@gmail.com>
2023-04-24 18:26:12 -0400

    knight moves function

--- knight.js
+++ knight.js
@@ -1,5 +1,7 @@
-function node(x,y){
-    return {x, y}
+function Node(position, path){
+    //current position
+    //array holding all previous positions
+    return {position, path };
 }
 
 function makeBoard(){
@@ -11,3 +13,37 @@ function makeBoard(){
         }
     }
 }
+
+function knightMoves([x,y], [endx, endy]){
+    let holder = [Node([x,y], [[x,y]])];
+    let currentNode = holder.shift();
+
+    while (currentNode.position[0] != endx || currentNode.position[1] != endy){
+        let moves = [
+            [currentNode.position[0] + 2, currentNode.position[1] - 1],
+            [currentNode.position[0] + 2, currentNode.position[1] + 1],
+            [currentNode.position[0] - 2, currentNode.position[1] - 1],
+            [currentNode.position[0] - 2, currentNode.position[1] + 1],
+            [currentNode.position[0] + 1, currentNode.position[1] - 2],
+            [currentNode.position[0] + 1, currentNode.position[1] + 2],
+            [currentNode.position[0] - 1, currentNode.position[1] - 2],
+            [currentNode.position[0] - 1, currentNode.position[1] + 2],
+        ];
+        moves.forEach((move) => {
+            let nextNode = Node(move, currentNode.path.concat([move]));
+            if (nextNode){
+                holder.push(nextNode)
+            }
+        });
+        currentNode = holder.shift()
+    }
+
+    let pathLength = currentNode.path.length -1;
+    console.log("You made it in " + pathLength + "moves! Heres's your path:");
+    currentNode.path.forEach((position) => {
+        console.log(position)
+    }
+    ); 
+}
+
+knightMoves([0,0],[3,3])
